d36a0f897e976ba76e9b584cdb9d0325b608c71c
“Amu-Y” <hakumai.morimori@gmail.com>
2021-05-03 14:02:32 +0000

    [Update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,17 @@
 class BooksController < ApplicationController
-  def show
-  end
-
   def new
+    @list = List.new
+  end
+  
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/home'
   end
 
-  def edit
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/show'
   get 'books/new'
-  get 'books/edit'
-  get 'homes/index'
-  get 'homes/edit'
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home' => 'homes#home_index.html'
+  post 'book' => 'books#create'
+  get 'book' => 'books#book_index.html'
 end
