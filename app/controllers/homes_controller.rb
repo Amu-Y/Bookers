2dac1d3258f4ffe67acbe67928fda4ac6d026a84
“Amu-Y” <hakumai.morimori@gmail.com>
2021-05-03 08:56:11 +0000

    [Update]

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,7 @@
+class HomesController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'books/show'
+  get 'books/new'
+  get 'books/edit'
+  get 'homes/index'
+  get 'homes/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
