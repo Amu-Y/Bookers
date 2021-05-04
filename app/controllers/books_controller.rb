317db62bac66418fa0f302c78625f073c9a38c2b
“Amu-Y” <hakumai.morimori@gmail.com>
2021-05-04 13:08:28 +0000

    [Update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,26 @@ class BooksController < ApplicationController
     @books = Book.all
     
   end
-
+  
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+  
+  def destroy
+        book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
 
   private
   def book_params
